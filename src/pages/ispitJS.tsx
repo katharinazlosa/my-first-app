cc37ea2f1555233d84d83e8f2dbfb14ec096ff35
grafika10 <grafika10@ucione.local>
2023-05-19 21:51:59 +0200

    new styles added

--- src/pages/ispitJS.tsx
+++ src/pages/ispitJS.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
+import Sound from "react-sound";
 
 type DataType = {
   trackName: string;
   artistName: string;
   trackId: number;
+  collectionName: string;
+  artworkUrl100: string;
+  previewUrl: string;
 };
 
 const IspitJS = () => {
@@ -38,51 +42,70 @@ const IspitJS = () => {
 
   return (
     <>
-      <div>
+      <div className="container">
         <div>
           <input
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
-            className="input"
+            className="ispit-input"
             type="text"
+            placeholder="search for songs..."
           />
           <button onClick={() => handleAdd()} className="btn">
             Dodaj
           </button>
         </div>
-        <div>
-          <table className="table-container">
-            <thead>
-              <tr className="table__content">
-                <th className="table__content">Artist</th>
-                <th className="table__content">Name of the song</th>
-              </tr>
-            </thead>
-            <tbody>
-              {!loading ? (
-                "Loading..."
+        <div className="table-container">
+          {!loading ? (
+            "Loading..."
+          ) : (
+            <>
+              {data.length > 0 ? (
+                data.map((result: DataType) => {
+                  return (
+                    <div key={result.trackId} className="ispit-container">
+                      <img src={result.artworkUrl100}></img>
+                      <div>
+                        <h2>{result.trackName}</h2>
+                        <h1>{result.artistName}</h1>
+                        <audio controls autoplay>
+                          <source
+                            src={result.previewUrl}
+                            type="audio/ogg"
+                          ></source>
+                        </audio>
+                      </div>
+                    </div>
+
+                    // <tr className="table__content" key={result.trackId}>
+                    //   <td className="table__content">
+                    //     {result.artistName}
+                    //   </td>
+                    //   <td className="table__content">{result.trackName}</td>
+                    //   <td className="table__content">
+                    //     {result.collectionName}
+                    //   </td>
+                    //   <td className="table__content">
+                    //     <img src={result.artworkUrl100}></img>
+                    //   </td>
+                    //   <td className="table__content">
+                    //     <audio controls autoplay>
+                    //       <source
+                    //         src={result.previewUrl}
+                    //         type="audio/ogg"
+                    //       ></source>
+                    //     </audio>
+                    //   </td>
+                    // </tr>
+                  );
+                })
               ) : (
-                <>
-                  {data.length > 0 ? (
-                    data.map((result: DataType) => {
-                      return (
-                        <tr className="table__content" key={result.trackId}>
-                          <td className="table__content">
-                            {result.artistName}
-                          </td>
-                          <td className="table__content">{result.trackName}</td>
-                        </tr>
-                      );
-                    })
-                  ) : (
-                    <tr>
-                      <td>Couldn't find what you're looking for. Try again!</td>
-                    </tr>
-                  )}
-                </>
+                <tr>
+                  <td>Couldn't find what you're looking for. Try again!</td>
+                </tr>
               )}
-            </tbody>
-          </table>
+            </>
+          )}
         </div>
       </div>
     </>
