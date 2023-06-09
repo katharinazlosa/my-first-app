99033297e08494882a50c963fe4ad35e92b6ed6d
Katharina <katharinazlosa@gmail.com>
2023-06-09 17:08:06 +0200

    delete btn added

--- src/pages/instagram/instagram.tsx
+++ src/pages/instagram/instagram.tsx
@@ -2,20 +2,20 @@ import { ChangeEvent, useEffect, useState } from "react";
 import { PostType } from "./types";
 import { v4 as uuidv4 } from "uuid";
 
+const newPostObj: PostType = {
+  id: "2",
+  title: "Swiss",
+  description: "Ovo je moje predobro putovanje u Švicarsku, bilo je odlično!",
+  hashtags: ["swiss", "travel", "hollidays", "influensanje"],
+  img_url:
+    "https://images.unsplash.com/photo-1527668752968-14dc70a27c95?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+};
+
 const Instagram = () => {
   const [posts, setPosts] = useState<PostType[]>([]);
   const [inputText, setInputText] = useState("");
   const [modal, setModal] = useState<boolean>();
-  const [remove, setRemove] = useState<boolean>();
-
-  const newPostObj: PostType = {
-    id: "2",
-    title: "Swiss",
-    description: "Ovo je moje predobro putovanje u Švicarsku, bilo je odlično!",
-    hashtags: ["swiss", "travel", "hollidays", "influensanje"],
-    img_url:
-      "https://images.unsplash.com/photo-1527668752968-14dc70a27c95?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-  };
+  const [like, setLike] = useState(false);
 
   const getPosts = () => {
     fetch("http://localhost:3000/instagram")
@@ -47,16 +47,28 @@ const Instagram = () => {
     const titleValue = document.querySelector("#title") as HTMLInputElement;
     const descValue = document.querySelector("#desc") as HTMLTextAreaElement;
     const imgValue = document.querySelector("#url") as HTMLInputElement;
+    const hashValue = document.querySelector("#hash") as HTMLInputElement;
 
     const newPost: PostType = {
       id: uuidv4(),
       title: titleValue.value,
       description: descValue.value,
-      hashtags: [],
+      hashtags: [hashValue.value],
       img_url: imgValue.value,
     };
 
     createPosts(newPost);
+    setModal(false);
+  };
+
+  const deletePost = (id: string) => {
+    fetch("http://localhost:3000/instagram/" + id, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        getPosts();
+      });
   };
 
   useEffect(() => {
@@ -67,9 +79,10 @@ const Instagram = () => {
     setInputText(e.target.value);
   };
 
-  const removeImg = () => {
-    setRemove(true);
+  const handleLike = () => {
+    setLike(!like);
   };
+
   return (
     <div>
       <div className="main-title">
@@ -94,34 +107,19 @@ const Instagram = () => {
               </div>
               <div className="input-field">
                 <label htmlFor="title">Title:</label>
-                <input
-                  type="text"
-                  id="title"
-                  placeholder="title"
-                  // value={inputText}
-                  // onChange={handleChange}
-                ></input>
+                <input type="text" id="title" placeholder="title"></input>
               </div>
               <div className="input-field">
                 <label htmlFor="title">Description:</label>
-                <input
-                  type="text"
-                  id="desc"
-                  placeholder="description"
-                  // value={inputText}
-                  // onChange={handleChange}
-                ></input>
+                <input type="text" id="desc" placeholder="description"></input>
               </div>
-
+              <div className="input-field">
+                <label htmlFor="title">Hashtags:</label>
+                <input type="text" id="hash" placeholder="hashtag"></input>
+              </div>
               <div className="input-field">
                 <label htmlFor="title">Image url:</label>
-                <input
-                  type="text"
-                  id="url"
-                  placeholder="img url"
-                  // value={inputText}
-                  // onChange={handleChange}
-                ></input>
+                <input type="text" id="url" placeholder="img url"></input>
               </div>
               <button className="input-field__btn" onClick={() => handlePost()}>
                 Post
@@ -144,7 +142,7 @@ const Instagram = () => {
         {posts.map((post: PostType) => {
           return (
             <>
-              <div className="picture-container">
+              <div className="picture-container" key={post.id}>
                 <div className="picture-container__header">
                   <img
                     src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
@@ -159,22 +157,39 @@ const Instagram = () => {
                     height="50"
                     src="https://img.icons8.com/ios/50/delete--v1.png"
                     alt="delete--v1"
-                    onClick={() => setRemove(true)}
+                    onClick={() => deletePost(post.id)}
                   />
                 </div>
                 <div className="picture-container__article">
                   <img src={post.img_url}></img>
                 </div>
                 <div className="picture-container__footer">
-                  <img
-                    className="
-                  picture-container__footer__img"
-                    width="50"
-                    height="50"
-                    src="https://img.icons8.com/ios/50/like--v1.png"
-                    alt="like--v1"
-                  />
-                  <h3>{"#" + post.hashtags.join("#")}</h3>
+                  <div>
+                    {" "}
+                    {like ? (
+                      <img
+                        className="
+                picture-container__footer__img"
+                        onClick={handleLike}
+                        width="50"
+                        height="50"
+                        src="https://img.icons8.com/emoji/48/heart-suit.png"
+                        alt="like--v1"
+                      />
+                    ) : (
+                      <img
+                        className="
+                      picture-container__footer__img"
+                        onClick={handleLike}
+                        width="48"
+                        height="48"
+                        src="https://img.icons8.com/ios/50/000000/like--v1.png"
+                        alt="heart-suit"
+                      />
+                    )}
+                  </div>
+
+                  <h3>{"#" + post.hashtags}</h3>
                 </div>
               </div>
             </>
